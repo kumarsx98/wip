07ee07294a7666457e96d97824252ee004c0945c
A, Meghana <meghana.a@abbvie.com>
2024-09-30 17:01:08 +0530

    "code changes for django assertion key context and login.js in react"

--- ous_agile_devops_ai/oad_ai/oad_ai_frontend/src/Login.js
+++ ous_agile_devops_ai/oad_ai/oad_ai_frontend/src/Login.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 function Login() {
-  const navigate = useNavigate();
-
   const handleLogin = () => {
     // Redirect to Django's SAML login URL
     window.location.href = 'http://localhost:8001/saml2/login/';
