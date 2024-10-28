1ba5be9fba06b07767bb1d7994bdd554d8e2e101
A, Meghana <meghana.a@abbvie.com>
2024-10-28 21:02:48 +0530

    "Removed the navigation pane for the chat with source comp"

--- ous_agile_devops_ai/oad_ai/oad_ai_frontend/src/App.js
+++ ous_agile_devops_ai/oad_ai/oad_ai_frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import ChatWithSource from './ChatWithSource';
@@ -21,24 +21,35 @@ const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/sources" element={<ListSources />} /> {/* Accessible without authentication */}
-          <Route path="/chat-with-source/:sourceName" element={<ChatWithSource />} /> {/* Accessible without authentication */}
-          <Route path="/media/previews/*" element={<FilePreview />} /> {/* Accessible without authentication */}
-
-          {/* Protected Routes */}
-          <Route path="/create-source" element={<ProtectedRoute component={CreateSource} />} />
-          <Route path="/auto-upload" element={<ProtectedRoute component={AutoUploadManager} />} />
-          <Route path="/websocket-test" element={<ProtectedRoute component={WebSocketComponent} />} />
-          <Route path="/sources/:sourceName/documents" element={<ProtectedRoute component={Documents} />} />
-          <Route path="/sources/:source/upload" element={<ProtectedRoute component={<div />} />} /> {/* Placeholder */}
-          <Route path="/delete-source" element={<ProtectedRoute component={DeleteSource} />} /> {/* Protected Delete Source */}
-        </Routes>
+        <MainLayout />
       </Router>
     </AuthProvider>
+  );
+};
+
+const MainLayout = () => {
+  const location = useLocation();
+  const isChatWithSource = location.pathname.startsWith('/chat-with-source');
+
+  return (
+    <div>
+      {!isChatWithSource && <Navigation />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sources" element={<ListSources />} /> {/* Accessible without authentication */}
+        <Route path="/chat-with-source/:sourceName" element={<ChatWithSource />} /> {/* Accessible without authentication */}
+        <Route path="/media/previews/*" element={<FilePreview />} /> {/* Accessible without authentication */}
+
+        {/* Protected Routes */}
+        <Route path="/create-source" element={<ProtectedRoute component={CreateSource} />} />
+        <Route path="/auto-upload" element={<ProtectedRoute component={AutoUploadManager} />} />
+        <Route path="/websocket-test" element={<ProtectedRoute component={WebSocketComponent} />} />
+        <Route path="/sources/:sourceName/documents" element={<ProtectedRoute component={Documents} />} />
+        <Route path="/sources/:source/upload" element={<ProtectedRoute component={<div />} />} /> {/* Placeholder */}
+        <Route path="/delete-source" element={<ProtectedRoute component={DeleteSource} />} /> {/* Protected Delete Source */}
+      </Routes>
+    </div>
   );
 };
 
